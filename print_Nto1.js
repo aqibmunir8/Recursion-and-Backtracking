18a1e82d1020201f32172a9a39e40f8ef14a6f25
Aqib Munir <aqibmunir8>
2024-10-12 22:56:43 +0500

    Printing N to 1 (2 :) )

--- print_Nto1.js
+++ print_Nto1.js
@@ -1,21 +1,21 @@
 
-// function print1toN (n){
+// function printNto1 (n){
 //     console.log(n)
 //     if(n>1){
-//         print1toN(n-1)
+//         printNto1(n-1)
 //     }
 //     console.log("Programm End")
 // }
 
 
 
-function print1toN (n){
+function printNto1 (n){
     if(n === 0){
         return;
     }
     console.log(n);
-    print1toN(n-1)
+    printNto1(n-1)
 }
 
 
-print1toN(5)
+printNto1(5)
